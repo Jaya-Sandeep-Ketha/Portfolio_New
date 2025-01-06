11e2682b65150c385f624c5d88f8f53fb0978150
Jaya-Sandeep-Ketha <kethajayasandeep1254@gmail.com>
2025-01-06 01:18:07 -0500

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,213 @@
+import jayasandeep from "./jayasandeep.png";
+import profilePic from "./profile-pic.jpg";
+import quotes from "./quotes.svg";
+import robot from "./robot.png";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import nitk from "./nitk.png";
+import cluboard from "./cluboard.png";
+import cash_flow from "./cash_flow.jpg";
+import freadom from "./freadom.jpg";
+import ecell from "./ecell.png";
+import bank from "./bank.jpg";
+import gdsc from "./gdsc.png";
+import iris from "./iris.png";
+import genesis from "./genesis.jpg";
+import todo from "./todo.jpg";
+import graphql from "./graphql.png";
+import portfolio from "./portfolio.jpg";
+import zulip from "./zulip.jpg";
+import publiclab from "./publiclab.png";
+import cdc from "./cdc.png";
+import chargeswap from "./chargeswap.png";
+import placeicon from "./place-icon.svg";
+import recruitment from "./recruitment.png";
+import huntly from "./huntly.png";
+import oracle from "./oracle.jpg";
+import comicify_ai from "./comicify-ai.png";
+import greentrust from "./green_trust.png";
+import averlon from "./averlon.jpg";
+import devfolio from "./devfolio.png";
+import pba from "./pba_logo.jpeg";
+import ethglobal from "./ethglobal.png";
+import polkadot from "./polkadot.jpeg";
+import lightspeed from "./lightspeed.png";
+import dennisivy from "./dennisivy.png";
+import manipal from "./manipal.png";
+import icon from "./icon.jpg";
+import ethforall from "./ethforall.png";
+import iitbhilai_motorsports from "./iitbhilaimotorsports.png";
+import robotics from "./robotics.png";
+import nss from "./nss.png";
+import iub from "./iub.png";
+import iitbh from "./iitbh.png";
+import iub_h2 from "./iub_h2.jpeg";
+import alumni from "./alumni.png";
+import nncvs from "./nncvs.png";
+import deloitte from "./deloitte.png";
+import fames from "./fames.png";
+import python from "./python.png";
+import java from "./java.png";
+import javascript from "./javascript.png";
+import typescript from "./typescript.png";
+import html from "./html.png";
+import css from "./css.png";
+import angular from "./angularjs.png";
+import react from "./react.png";
+import nodejs from "./nodejs.png";
+import expressjs from "./expressjs.png";
+import springboot from "./springboot.png";
+import django from "./django.png";
+import jquery from "./jquery.png";
+import bootstrap from "./bootstrap.png";
+import tailwindcss from "./tailwind.png";
+import materialui from "./materialui.png";
+import aws from "./aws.png";
+import azure from "./azure.png";
+import postgresql from "./postgres.png";
+import mysql from "./mysql.png";
+import mongodb from "./mongodb.png";
+import redis from "./redis.png";
+import kafka from "./kafka.png";
+import docker from "./docker.png";
+import kubernetes from "./kubernetes.png";
+import jenkins from "./jenkins.png";
+import terraform from "./terraform.png";
+import linux from "./linux.png";
+import git from "./git.png";
+import github from "./github.png";
+import postman from "./postman.png";
+import spark from "./spark.png";
+import hadoop from "./hadoop.png";
+import hive from "./hive.png";
+import powerbi from "./powerbi.png";
+import tableau from "./tableau.png";
+import snowflake from "./snowflake.png";
+import excel from "./excel.png";
+import pandas from "./pandas.png";
+import matplotlib from "./matplotlib.png";
+import numpy from "./numpy.png";
+import scikitlearn from "./scikitlearn.png";
+import pytorch from "./pytorch.png";
+import keras from "./keras.png";
+import tensorflow from "./tensorflow.png";
+import opencv from "./opencv.png";
+
+export {
+  quotes,
+  robot,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  nitk,
+  cluboard,
+  cash_flow,
+  freadom,
+  bank,
+  gdsc,
+  iris,
+  ecell,
+  genesis,
+  todo,
+  graphql,
+  portfolio,
+  publiclab,
+  zulip,
+  cdc,
+  jayasandeep,
+  profilePic,
+  chargeswap,
+  placeicon,
+  recruitment,
+  huntly,
+  oracle,
+  comicify_ai,
+  greentrust,
+  averlon,
+  devfolio,
+  pba,
+  ethglobal,
+  polkadot,
+  lightspeed,
+  dennisivy,
+  manipal,
+  icon,
+  ethforall,
+  iitbhilai_motorsports,
+  robotics,
+  nss,
+  iub,
+  iitbh,
+  iub_h2,
+  alumni,
+  nncvs,
+  deloitte,
+  fames,
+  python,
+  java,
+  javascript,
+  typescript,
+  html,
+  css,
+  angular,
+  react,
+  nodejs,
+  expressjs,
+  springboot,
+  django,
+  jquery,
+  bootstrap,
+  tailwindcss,
+  materialui,
+  aws,
+  azure,
+  postgresql,
+  mysql,
+  mongodb,
+  redis,
+  kafka,
+  docker,
+  kubernetes,
+  jenkins,
+  terraform,
+  linux,
+  git,
+  github,
+  postman,
+  spark,
+  hadoop,
+  hive,
+  powerbi,
+  tableau,
+  snowflake,
+  excel,
+  pandas,
+  matplotlib,
+  numpy,
+  scikitlearn,
+  pytorch,
+  keras,
+  tensorflow,
+  opencv,
+};
